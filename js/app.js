5aeb9cb89146f83c881b06cfc469de6b79b6009c
Juan Diego González Garcia <njuand2g@gmail.com>
2020-07-03 18:38:25 -0500

    Primeros archivos de la pagina de la banda

---
+++ js/app.js
@@ -0,0 +1,57 @@
+function clear (){
+    var element = document.getElementById("Inicio");
+    element.classlist.add("hidden");
+    var element = document.getElementById("Integrantes");
+    element.classlist.add("hidden");
+    var element = document.getElementById("Canciones");
+    element.classlist.add("hidden");
+    var element = document.getElementById("Videos");
+    element.classlist.add("hidden");
+    var element = document.getElementById("Arte");
+    element.classlist.add("hidden");
+    var element = document.getElementById("Contactos");
+    element.classlist.add("hidden");
+    var element = document.getElementById("Comentarios");
+    element.classlist.add("hidden");
+}
+
+function showInicio(){
+    clear ()
+
+
+}
+
+function showIntegrantes(){
+    clear ()
+
+    
+}
+
+function showCanciones(){
+    clear ()
+
+
+}
+
+function showVideos (){
+    clear ()
+
+
+}
+
+function showArte(){ 
+    clear ()
+
+}
+
+function showContactos(){
+    clear ()
+
+
+}
+
+function showComentarios(){
+    clear ()
+
+
+}
